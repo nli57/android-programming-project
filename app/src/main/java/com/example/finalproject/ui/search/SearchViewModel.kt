82af5c92d52822ababb28737e8d78459d91ee551
Nancy Li <nancyli@Nancys-MacBook-Pro.local>
2022-11-13 00:36:18 -0800

    update navigation

--- app/src/main/java/com/example/finalproject/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/finalproject/ui/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.finalproject.ui.home
+package com.example.finalproject.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is search Fragment"
     }
     val text: LiveData<String> = _text
 }
